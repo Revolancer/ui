f9ad6afadc6011415c92d123bd55b237a2f528ba
Alfonso Vittorio Salvador <86940949+ponkeeto@users.noreply.github.com>
2023-08-16 06:19:20 +0000

    add collapsible components

---
+++ src/collapsible/index.ts
@@ -0,0 +1,14 @@
+import { styled } from '../styles';
+
+export const Expander = styled('div', {
+  borderRadius: '100%',
+  borderStyle: '$solid',
+  borderWidth: '$1',
+  borderColor: '$neutral500',
+  width: '$8',
+  height: '$8',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: '$neutral600',
+});
