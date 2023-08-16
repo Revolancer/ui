4b4fd5a3d801d7e9c2b86b0f1bfe3d78e9b7f1ca
Alfonso Vittorio Salvador <86940949+ponkeeto@users.noreply.github.com>
2023-08-16 06:20:11 +0000

    add tags components

---
+++ src/tags/index.ts
@@ -0,0 +1,12 @@
+import { styled } from '../styles';
+
+export const TagContainer = styled('div', {
+  color: '$pink500',
+  padding: '$1 $4',
+  borderRadius: '100px',
+  background: '$pink100',
+});
+
+//admin-page-tags
+export const TH = styled('th', { border: '1px solid black', padding: '$1' });
+export const TD = styled('td', { border: '1px solid black', padding: '$1' });
