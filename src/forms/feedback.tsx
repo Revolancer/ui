2b8b9007b918efcbdaaa2182a1ac505c3838a3bb
Alfonso Vittorio Salvador <86940949+ponkeeto@users.noreply.github.com>
2023-08-17 09:46:52 +0000

    add feedback component

---
+++ src/forms/feedback.tsx
@@ -0,0 +1,32 @@
+// import {
+// faCircleCheck,
+// faCircleInfo,
+//   faTriangleExclamation,
+// } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FeedbackText, FeedbackTextAfter } from ".";
+
+export const Feedback = ({
+  state = 'warning',
+  children,
+}: {
+  state?: 'feedback' | 'warning' | 'error' | 'success';
+  children?: any;
+}) => {
+  /*let symbol = faCircleInfo;
+  switch (state) {
+    case 'warning':
+    case 'error':
+      symbol = faTriangleExclamation;
+      break;
+    case 'success':
+      symbol = faCircleCheck;
+      break;
+  }*/
+  return (
+    <FeedbackText state={state}>
+      {/*<FontAwesomeIcon icon={symbol} />*/}
+      <FeedbackTextAfter>{children}</FeedbackTextAfter>
+    </FeedbackText>
+  );
+};
