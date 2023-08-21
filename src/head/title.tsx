9ba03fdb367dc456ebf0abe9522e672afccc10e0
Alfonso Vittorio Salvador <86940949+ponkeeto@users.noreply.github.com>
2023-08-21 14:34:36 +0000

    delete head folder

--- src/head/title.tsx
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head';
-
-export const Title = ({ children }: { children: string }) => {
-  return (
-    <Head>
-      <title>{`${children} - Revolancer`}</title>
-    </Head>
-  );
-};
