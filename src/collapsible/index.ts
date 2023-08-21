1ba0b83343089d8488f4da7bce503049e68e8bb9
Alfonso Vittorio Salvador <86940949+ponkeeto@users.noreply.github.com>
2023-08-21 14:33:44 +0000

    added collapsible folder barrel

--- src/collapsible/index.ts
+++ src/collapsible/index.ts
@@ -1,14 +1,1 @@
-import { styled } from '../styles';
-
-export const Expander = styled('div', {
-  borderRadius: '100%',
-  borderStyle: '$solid',
-  borderWidth: '$1',
-  borderColor: '$neutral500',
-  width: '$8',
-  height: '$8',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  color: '$neutral600',
-});
+export * from './explainer'
