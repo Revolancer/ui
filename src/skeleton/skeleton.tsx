67ef62aa20d8c0fbec89a8a6e87ab936577488ef
Alfonso Vittorio Salvador <86940949+ponkeeto@users.noreply.github.com>
2023-09-13 01:30:38 +0000

    fix skeleton width bug bump to version 1.2.2

--- src/skeleton/skeleton.tsx
+++ src/skeleton/skeleton.tsx
@@ -4,35 +4,29 @@ const SkeletonText = styled('div', {
   ...loadingStyles,
   borderRadius: '$1',
   position: 'relative',
+  width: '100%',
   variants: {
     type: {
       h1: {
         height: '$16',
-        minWidth: '300px',
       },
       h2: {
         height: '$13',
-        minWidth: '300px',
       },
       h3: {
         height: '$11',
-        minWidth: '300px',
       },
       h4: {
         height: '$8',
-        minWidth: '300px',
       },
       h5: {
         height: '$6',
-        minWidth: '300px',
       },
       p: {
         height: '$6',
-        minWidth: '300px',
       },
       span: {
         height: '$5',
-        minWidth: '300px',
       },
       tag: {
         height: '$8',
@@ -41,7 +35,6 @@ const SkeletonText = styled('div', {
       },
       image: {
         height: '200px',
-        minWidth: '300px',
         overflow: 'hidden',
         borderRadius: '0px',
       },
